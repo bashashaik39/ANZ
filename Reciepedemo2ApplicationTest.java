1cfb16bd92ed8843dfefe70335398e30481e5b4d
bashashaik39 <47564651+bashashaik39@users.noreply.github.com>
2019-04-03 01:08:41 +0530

    Add files via upload

--- Reciepedemo2ApplicationTest.java
+++ Reciepedemo2ApplicationTest.java
@@ -7,10 +7,14 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import org.junit.Before;
 import org.junit.FixMethodOrder;
 import org.junit.Test;
+import static org.mockito.Mockito.times;
+import static org.hamcrest.Matchers.is;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
 import org.mockito.MockitoAnnotations;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -18,6 +22,7 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
@@ -30,47 +35,59 @@ import com.reciepe.demo2.Reciepedemo2.Service.ReciepeService;
 public class Reciepedemo2ApplicationTest {
 
 	private MockMvc mockMvc;
+
 	@Mock
-    private ReciepeDao daoMock;
-    @InjectMocks
-    private ReciepeService service;
+	private ReciepeService service;
 
 	@Autowired
-    private WebApplicationContext wac;
-    @Before
-    public void setUp() throws Exception {
-        MockitoAnnotations.initMocks(this);
-    }
- 
+	private WebApplicationContext wac;
+
 	@Before
 	public void setup() {
-        this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
-            MockitoAnnotations.initMocks(this);
+		this.mockMvc = MockMvcBuilders.webAppContextSetup(wac).build();
+		MockitoAnnotations.initMocks(this);
 	}
+
 	
 	@Test
 	public void verifyAllToDoSave() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.get("/save").accept(MediaType.APPLICATION_JSON));
-	}
-	@Test
-	public void verifyAllToDoList() throws Exception {
-		mockMvc.perform(MockMvcRequestBuilders.get("/get").accept(MediaType.APPLICATION_JSON))
-			.andExpect(jsonPath("$", hasSize(3))).andDo(print());
+
 	}
 	 
+
+	@Test
+	public void verifyAllToDoList() throws Exception {
+		mockMvc.perform(MockMvcRequestBuilders.get("/get"))
+				.andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON_UTF8_VALUE))
+				.andExpect(jsonPath("$", hasSize(3))).andExpect(jsonPath("$[0].id", is(1)))
+				.andExpect(jsonPath("$[1].id", is(2))).andExpect(jsonPath("$[2].id", is(3)))
+				.andExpect(jsonPath("$[0].title", is("Creamy Scrambled Eggs Recipe Recipe")))
+				.andExpect(jsonPath("$[1].title", is("Creamy Scrambled Eggs Recipe Recipe")))
+				.andExpect(jsonPath("$[2].title", is("Creamy Scrambled Eggs Recipe Recipe")))
+				.andExpect(jsonPath("$[0].thumbnail", is("http://img.recipepuppy.com/ 373064.jpg")))
+				.andExpect(jsonPath("$[1].thumbnail", is("http://img.recipepuppy.com/ 373064.jpg")))
+				.andExpect(jsonPath("$[2].thumbnail", is("http://img.recipepuppy.com/ 373064.jpg")))
+				.andExpect(jsonPath("$[0].href", is("http://www.grouprecipes.com/43522/creamyscrambled-eggs-recipe.html")))
+				.andExpect(jsonPath("$[1].href", is("http://www.grouprecipes.com/43522/creamyscrambled-eggs-recipe.html")))
+				.andExpect(jsonPath("$[2].href", is("http://www.grouprecipes.com/43522/creamyscrambled-eggs-recipe.html")));
+		verify(service, times(0)).getreciepe();
+		verifyNoMoreInteractions(service);
+	}
+
 	@Test
 	public void verifyToDoById() throws Exception {
 		mockMvc.perform(MockMvcRequestBuilders.get("/getbyid/1").accept(MediaType.APPLICATION_JSON))
-		.andExpect(jsonPath("$.id").exists())
-		.andExpect(jsonPath("$.title").exists())
-		.andExpect(jsonPath("$.href").exists())
-		.andExpect(jsonPath("$.ingredients").exists())
-		.andExpect(jsonPath("$.id").value(1))
-		.andExpect(jsonPath("$.title").value("Creamy Scrambled Eggs Recipe Recipe"))
-		.andExpect(jsonPath("$.ingredients").isArray())
-		.andExpect(jsonPath("$.href").value("http://www.grouprecipes.com/43522/creamyscrambled-eggs-recipe.html"))
-		.andDo(print());
+				.andExpect(jsonPath("$.id").exists())
+				.andExpect(jsonPath("$.title").exists())
+				.andExpect(jsonPath("$.href").exists())
+				.andExpect(jsonPath("$.ingredients").exists())
+				.andExpect(jsonPath("$.id").value(1))
+				.andExpect(jsonPath("$.title").value("Creamy Scrambled Eggs Recipe Recipe"))
+				.andExpect(jsonPath("$.ingredients").isArray())
+				.andExpect(
+						jsonPath("$.href").value("http://www.grouprecipes.com/43522/creamyscrambled-eggs-recipe.html"))
+				.andDo(print());
 	}
 	
-	 
-	}
+}
